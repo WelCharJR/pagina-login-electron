6b458d7ace0af84f15cc7a8a38504b16a6e3fb7f
Welington <welingtoncharnetjr@gmail.com>
2025-05-05 20:41:13 -0300

    Criando arquivo mainJs

---
+++ main.js
@@ -0,0 +1,15 @@
+//importando os modulos app e BrowserWindow do Electron
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+})
